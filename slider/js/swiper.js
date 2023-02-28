fcf7e87152a4c8ea6eb0c7f2d4803a9aebeef9dd
Lyuba Bessarab <bessarab97@gmail.com>
2023-02-28 15:02:03 +0300

    swiper tune

--- slider/js/swiper.js
+++ slider/js/swiper.js
@@ -1,15 +1,31 @@
-new Swiper('.mySwiper', {
+new Swiper ('.mySwiper', {
     slidesPerView: "auto",
+    simulateTouch: true,
+    tuochRatio:50,
+    touchAngle:45,
+    grabCursor:true,
     spaceBetween: 30,
-    mousewheel: true,
-    keyboard: true,
     pagination: {
-      el: '.swiperPagination',
-    clickable: true,
-    mousewheel: true,
-    keyboard: true,
-    },
-  });
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    }
+});
+
+
+
+// new Swiper('.mySwiper', {
+//     slidesPerView: "auto",
+//     spaceBetween: 30,
+//     mousewheel: true,
+//     keyboard: true,
+//     pagination: {
+//       el: '.swiperPagination',
+//     clickable: true,
+//     mousewheel: true,
+//     keyboard: true,
+//     },
+//   });
 
 
 // if (window.matchMedia("(max-width: 500px)").matches) { 
